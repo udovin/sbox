d4373800557c9b86f4e4a63624f1248e132cabba
Ivan Udovin <wilcot@ya.ru>
2024-02-04 17:49:49 +0300

    Add some logging

--- tests/manager.rs
+++ tests/manager.rs
@@ -1,6 +1,45 @@
-use std::path::PathBuf;
+use std::{
+    io::ErrorKind,
+    path::{Path, PathBuf},
+};
 
+use rand::distributions::{Alphanumeric, DistString};
 use sbox::{ContainerConfig, Error, Manager, ProcessConfig};
+
+pub struct TempDir(PathBuf);
+
+impl TempDir {
+    #[allow(unused)]
+    pub fn join<P: AsRef<Path>>(&self, path: P) -> PathBuf {
+        self.0.join(path)
+    }
+
+    #[allow(unused)]
+    pub fn as_path(&self) -> &Path {
+        self.0.as_path()
+    }
+}
+
+impl Drop for TempDir {
+    fn drop(&mut self) {
+        std::fs::remove_dir_all(&self.0).unwrap();
+    }
+}
+
+#[allow(unused)]
+pub fn temp_dir() -> Result<TempDir, Error> {
+    let tmpdir = Path::new(env!("CARGO_TARGET_TMPDIR"));
+    let path = loop {
+        let path = tmpdir.join(Alphanumeric.sample_string(&mut rand::thread_rng(), 32));
+        match std::fs::metadata(&path) {
+            Ok(_) => continue,
+            Err(v) if v.kind() == ErrorKind::NotFound => break path,
+            Err(v) => return Err(v.into()),
+        }
+    };
+    std::fs::create_dir_all(&path)?;
+    Ok(TempDir(path))
+}
 
 fn get_rootfs() -> Result<PathBuf, Error> {
     let mut child = std::process::Command::new("/bin/sh")
@@ -35,9 +74,14 @@ fn get_cgroup() -> Result<PathBuf, Error> {
 
 #[test]
 fn test_manager() {
+    let tmpdir = temp_dir().unwrap();
     let rootfs = get_rootfs().unwrap();
     let cgroup = get_cgroup().unwrap();
-    let manager = Manager::new("/tmp/sbox", cgroup.clone()).unwrap();
+    let state_dir = tmpdir.join("state");
+    println!("Rootfs path: {:?}", rootfs);
+    println!("Cgroup path: {:?}", cgroup);
+    println!("State path: {:?}", state_dir);
+    let manager = Manager::new(state_dir, cgroup.clone()).unwrap();
     let container_config = ContainerConfig {
         layers: vec![rootfs],
         ..Default::default()
